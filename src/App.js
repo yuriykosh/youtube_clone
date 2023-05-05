870f71315aacd309b28f2e9b71f7a89d3d1ff0d2
Yuriy Koshyk <yuriy.koshyk@gmail.com>
2023-05-05 21:27:39 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from './components/index';
+
+const App = () => {
+  <Box sx={{ backgroundColor: '#000' }} height="150px">
+    <>hello</>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Feed />} />
+      <Route path="/video/:id" element={<VideoDetail />} />
+      <Route path="/channel/:id" element={<ChannelDetail />} />
+      <Route path="/search/:searchTerm" element={<SearchFeed />} />
+    </Routes>
+  </Box>;
+};
 
 export default App;
